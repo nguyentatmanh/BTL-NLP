77428352459dd4ac6783898210c3ee59ead2c484
nguyentatmanh <167743345+nguyentatmanh@users.noreply.github.com>
2026-04-03 10:58:17 +0700

    chore: minor updates to files before final submission

--- viet_qa/viet_qa/test_load.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
-
-model_path = r"D:\BTL NLP\viet_qa\src\viet_qa\checkpoints\extractive"
-
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForQuestionAnswering.from_pretrained(model_path)
-
-print("Load model thành công!")
